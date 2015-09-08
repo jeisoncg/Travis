7b15fa58d229b39151cce5561d8937f08f939af0
Jeison C <jeisoncardona@outlook.com>
2015-09-08 00:20:42 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Vista.cpp"
+
+int main(){
+	
+	
+	Vista  x;
+	x.menu();
+	
+	return 0;
+	
+	}
---
+++ main_http.cpp
@@ -0,0 +1,10 @@
+#include "http.cpp"
+
+int main (int argc, char *argv[])
+
+{
+	http h;
+  
+    h.descargar_sinonimos_completos();
+	return 0;
+}
