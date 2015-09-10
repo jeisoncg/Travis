6d6730224bae4b3b7f249eaba68bda35fb347c0b
Jeison C <jeisoncardona@outlook.com>
2015-09-10 00:07:40 -0500

    Update general

--- src/main_http.cpp
+++ src/main_http.cpp
@@ -1,11 +1,14 @@
 #include "Controladores/http.cpp"
+#include <iomanip>
+using std::setw;
 
 int main (int argc, char *argv[])
 
 {
-	http h;
-    h.descargar_sinonimos_completos();
-    
-    
+	//http h;
+    //h.descargar_sinonimos_completos();
+    Proxy x;
+    x.ping("www.google.com");
+    cout<<x.ping("www.goosdgle.com");
 	return 0;
 }
