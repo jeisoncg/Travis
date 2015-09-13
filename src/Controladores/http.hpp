e96d7416a80ee0bbf0434d3f380d73f20e52a20c
luchoman <luchoman08@gmail.com>
2015-09-13 09:07:28 -0500

    cambio 13/09/2015
    se modifica la clase http adicionando nuevas variables de entorno y haciendo mas facil de usar desde la clase vista, se ha eliminado el metodo de descargar todos los html, siendo dejado el solo descargar html por palabra

--- src/Controladores/http.hpp
+++ src/Controladores/http.hpp
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <vector>
-#include "Proxy.cpp"
+
 
 using std::vector;
 
@@ -14,26 +14,31 @@ class http{
 	std::string encabezadoComando; //indica la pagina principal de la que extraera la informacion
 	std::string palabraComando; //indica la palabra a consultar
 	std::string extencionComando; //indica la extencion de la pagina (html, php, aspx...)
-	std::string comando;
-	Proxy *proxies;
+	std::string comando;//es la suma de los argumentos y el comando, el cual se ejecutara en system
+	std::string proxy_actual; //es el proxy el cual se esta utilizando en el comando aria2c
+	
 
+   
 	
 	
 	public:
-	void cargar_archivo();
+	http();
+	~http();
 	void set_encabezado(std::string encabezado);
     void set_palabra(std::string palabra);
     void set_extencion(std::string extencion);
+    void set_proxy_actual(std::string nuevo_proxy);
     std::string getEncabezado();
     std::string get_palabra();
     std::string get_extencion();
-	http(Proxy *proxies);
-	~http();
-	int descargar_html(std::string palabra,std::string proxyURL);
+    std::string get_comando();
+    std::string get_proxy_actual();
+	int descargar_html(std::string palabra);
 	void descargar_sinonimos_completos();
-	void construir_comando(std::string proxyURL); //construlle el comando con base en el encabezado, la palabra a consulatar y la extencion
+	void construir_comando(); //construlle el comando con base en el encabezado, la palabra a consulatar y la extencion
 	
 };
 
 
 #endif
+
