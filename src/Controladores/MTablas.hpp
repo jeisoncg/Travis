efd39148d3f3ece3c74116e17e25f8fe6a732431
Jeison <jeisoncardona@outlook.com>
2015-09-16 17:04:29 -0500

    Lector de tablas desde archivo de HTML

---
+++ src/Controladores/MTablas.hpp
@@ -0,0 +1,22 @@
+#ifndef MTablas_hpp
+#definde MTablas_hpp
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+class MTablas{
+	
+	
+	private:
+	
+	std::vector <std::string> html;
+	
+	public:
+	
+	
+	};
+
+#endif
+
